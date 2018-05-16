28ad88cce4788f6d5dbd42cb7f1615d542f7c2e5
EJ Fox <ejfox@ejfox.com>
2018-05-15 23:26:38 -0400

    Add basic progress bar

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -2,13 +2,18 @@
   <div class="">
     <div class="f3 pv3 f6 mb2 db ttu tracked o-40 barlowcondensed">Total duration: {{ Math.round(duration / 60)  }} minutes</div>
     <div class="f3 pv3">Progress: {{ (progress * 100) }}%</div>
+    <div class="progress-bar"
+      :style="{width: (progress * 100)+'%'}">
+
+    </div>
     <div class="controls">
-      <button class="f1 link dim ph3 pa4 mb2 dib white bg-black w-100"
+      <a class="f1 tc ph3 pa4 mb2 dib white bg-black w-100 br-pill"
+      style="color: white !important;"
       @click="togglePlayback">
         <!-- {{ playing ? 'Pause' : 'Play' }} -->
         <ion-icon
         :name="playing ? 'Pause' : 'Play'"></ion-icon>
-      </button>
+      </a>
       <!-- <button class="f6 link dim ph3 pv2 mb2 dib white bg-black w-50"
       @click="stop">Stop</button> -->
     </div>
@@ -32,6 +37,16 @@ export default {
   }
 };
 </script>
+<style>
+.progress-bar {
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  background-color: red;
+}
+</style>
 
 
 <style>

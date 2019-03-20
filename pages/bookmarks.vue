aeb71637fc34491697e362aa8927c63f627275f2
EJ Fox <ejfox@ejfox.com>
2019-03-19 23:41:28 -0400

    fix bookmarks

--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,58 +1,49 @@
 <template>
   <section class="center db f3 cf tc">
-    <section id="header">
-      <small class="db">
-        <a href="https://ejfox.com">&lt;- ejfox.com</a>
-      </small>
-    </section>
-
     <section v-if="!blocks">
       <h2>Loading!</h2>
     </section>
 
-    <h2 class="mv4">Recent Bookmarks</h2>
+    <h2 class="mv4 moon-gray">Recent Bookmarks</h2>
 
-    <section v-for="block in blocks" :key="block.$.hash" class="sans-serif mb4 tl dib v-top w-100 w-third-l pa3 pa4-l pb0 lh-copy overflow-scroll f4">
-      <a :href="block.$.href">
-        <span class="o-20">
-          <i v-if="linkDomain(block.$.href) === 'youtube.com'" :class="['fab fa-youtube']" />
-          <i v-else-if="linkDomain(block.$.href) === 'twitter.com'" :class="['fab fa-twitter']" />
-          <i v-else-if="linkDomain(block.$.href) === 'instagram.com'" :class="['fab fa-instagram']" />
-          <i v-else-if="linkDomain(block.$.href) === 'reddit.com'" :class="['fab fa-reddit']" />
-          <i v-else-if="linkDomain(block.$.href) === 'twitch.com'" :class="['fab fa-twitch']" />
-          <i v-else-if="linkDomain(block.$.href) === 'vimeo.com'" :class="['fas fa-vimeo']" />
-          <i v-else-if="linkDomain(block.$.href) === 'twitch.com'" :class="['fab fa-twitch']" />
-          <i v-else-if="linkDomain(block.$.href) === 'medium.com'" :class="['fab fa-medium-m']" />
-          <i v-else-if="linkDomain(block.$.href) === 'codepen.com'" :class="['fab fa-codepen']" />
-          <i v-else-if="linkDomain(block.$.href) === 'dribbble.com'" :class="['fab fa-dribbble']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('video') > 0" :class="['fas fa-video']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('github') > 0" :class="['fab fa-github-alt']" />
-          <!-- <i v-else-if="block.$.tag.split(' ').indexOf('watercolor') > 0" :class="['fas fa-paint-brush']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('painting') > 0" :class="['fas fa-paint-brush']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('art') > 0" :class="['fas fa-paint-brush']" /> -->
-          <i v-else-if="block.$.tag.split(' ').indexOf('machinelearning') > 0" :class="['fas fa-cog']" />
-          <!-- <i v-else-if="block.$.tag.split(' ').indexOf('music') > 0" :class="['fas fa-music']" /> -->
-          <i v-else-if="block.$.tag.split(' ').indexOf('technique') > 0" :class="['fas fa-book']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('howto') > 0" :class="['fas fa-book']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('resource') > 0" :class="['fas fa-book']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('reference') > 0" :class="['fas fa-book']" />
-          <i v-else-if="block.$.tag.split(' ').indexOf('facebook') > 0" :class="['fab fa-facebook']" />
-        </span>
+    <section v-for="(block, i) in blocks" :key="block.u" class="sans-serif mb4 tl dib v-top w-100 w-third-l pa3 pa4-l pb0 lh-title overflow-scroll f2">
+      <a :href="block.u"
+        class="lh-title dark-gray link">
+        <div class="b ttu">
+          {{block.d}}
+        </div>
 
-        <span class="b lh-title ttu">
-          {{block.$.description}}
-        </span>
+        <!-- <span class="o-20">
+          <i v-if="linkDomain(block.u) === 'youtube.com'" :class="['fab fa-youtube']" />
+          <i v-else-if="linkDomain(block.u) === 'twitter.com'" :class="['fab fa-twitter']" />
+          <i v-else-if="linkDomain(block.u) === 'instagram.com'" :class="['fab fa-instagram']" />
+          <i v-else-if="linkDomain(block.u) === 'reddit.com'" :class="['fab fa-reddit']" />
+          <i v-else-if="linkDomain(block.u) === 'twitch.com'" :class="['fab fa-twitch']" />
+          <i v-else-if="linkDomain(block.u) === 'vimeo.com'" :class="['fas fa-vimeo']" />
+          <i v-else-if="linkDomain(block.u) === 'twitch.com'" :class="['fab fa-twitch']" />
+          <i v-else-if="linkDomain(block.u) === 'medium.com'" :class="['fab fa-medium-m']" />
+          <i v-else-if="linkDomain(block.u) === 'codepen.com'" :class="['fab fa-codepen']" />
+          <i v-else-if="linkDomain(block.u) === 'dribbble.com'" :class="['fab fa-dribbble']" />
+          <i v-else-if="block.t.indexOf('video') > 0" :class="['fas fa-video']" />
+          <i v-else-if="block.t.indexOf('github') > 0" :class="['fab fa-github-alt']" />
+          <i v-else-if="block.t.indexOf('machinelearning') > 0" :class="['fas fa-cog']" />
+          <i v-else-if="block.t.indexOf('technique') > 0" :class="['fas fa-book']" />
+          <i v-else-if="block.t.indexOf('howto') > 0" :class="['fas fa-book']" />
+          <i v-else-if="block.t.indexOf('resource') > 0" :class="['fas fa-book']" />
+          <i v-else-if="block.t.indexOf('reference') > 0" :class="['fas fa-book']" />
+          <i v-else-if="block.t.indexOf('facebook') > 0" :class="['fab fa-facebook']" />
+        </span> -->
 
-        <small class="db ttu f8 o-20 mt1">
-          {{block.$.tag}}
+        <small v-if="block.t.length > 1" v-for="tag in block.t" class="ttu f8 o-20 mv0 mr1">
+          {{tag}}
         </small>
 
-        <small v-if="!block.$.extended" class="db f8 o-20 word-wrap">
-          {{block.$.href}}
+        <small v-if="!block.n" class="db f6 word-wrap moon-gray">
+          {{block.u}}
         </small>
 
         <div class="measure f5 lh-copy bookmark-description">
-          <small v-if="block.$.extended" class="bg-light-yellow i">{{block.$.extended}}</small>
+          <small v-if="block.n" class="bg-light-yellow i">{{block.n}}</small>
         </div>
       </a>
     </section>
@@ -80,7 +71,8 @@ import * as URI from 'uri-js'
 const parseString = xml2js.parseString
 const stripPrefix = xml2js.processors.stripPrefix;
 
-const pinboardURI = 'https://api.pinboard.in/v1/posts/all?auth_token=ejfox:6BCADA7AD389C5F5D7CE&results=50'
+// const pinboardURI = 'https://api.pinboard.in/v1/posts/all?auth_token=ejfox:6BCADA7AD389C5F5D7CE&results=50'
+const pinboardURI = 'https://pinboard-api.now.sh/json/u:ejfox/?results=50'
 
 export default {
   components: {
@@ -95,7 +87,7 @@ export default {
   mounted: function () {
     axios.get(pinboardURI)
     .then((res) => {
-      this.blocks = this.parseXML(res.data)
+      this.blocks = res.data
     })
   },
   methods: {
@@ -114,13 +106,7 @@ export default {
   asyncData ({ params }) {
     return axios.get(pinboardURI)
     .then((res) => {
-      let parsedXMLContent
-      const parsedXML = parseString(res.data, { tagNameProcessors: [ stripPrefix ] }, (err, result) => {
-        const links = result.posts.post
-        parsedXMLContent = { blocks: links }
-      })
-
-      return parsedXMLContent
+      return { blocks: res.data }
     })
   }
 };

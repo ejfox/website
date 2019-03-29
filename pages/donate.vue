9940a39803242583e8105857f366b5e12950756e
EJ Fox <ejfox@ejfox.com>
2019-03-29 01:06:04 -0400

    update donate page

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,21 +1,42 @@
 <template>
-  <section class="w-80 center tc pa5 f3">
+  <section class="w-80-ns center tc pa3 pa5-ns f3">
+
+    <div class="measure-ns center mv4">
+      <p>Your support of <b>$1</b>, <b>$5</b>, or <b>$20</b> helps me have more time and resources to make cool shit.</p>
+      <!-- <p class="mt4">We all like more cool shit.</p> -->
+    </div>
+
+    <div class="mb5">
+      <h2>Cash app: <a class="link green underline"href="https://cash.me/$ejfox">$ejfox</a></h2>
+    </div>
+
     <div>
       <a class="donate-with-crypto"
          href="https://commerce.coinbase.com/checkout/b33a499d-e032-4554-967c-2353d15d8651">
-        <span>Donate to EJ Fox with cryptocurrency</span>
+        <span>Donate to EJ Fox with cryptocurrency through Coinbase</span>
       </a>
       <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807">
       </script>
     </div>
 
-    <h2 class="mv5">OR</h2>
+    <!-- <h2 class="mv5">OR</h2> -->
 
-    <h3>Send directly to BTC address</h3>
+    <h3 class="mt5">Send directly to BTC address</h3>
+    <h4 class="courier ttn bg-dark-gray white f2 pa2 br2 break-word mv2">3DE42VUyUKSikQ9eUeFKv2EkKVms7Pmd1G</h4>
     <img src="/coinbase-bitcoin.png" class="w-100 w-50-ns" alt="">
 
-    <h3>Send directly to BCH address</h3>
+    <h3 class="mt5">Send directly to BCH address</h3>
+    <h4 class="courier ttn bg-dark-gray white f2 pa2 br2 break-word mv2">qrwzlfjhhn8jdx92a8m8e2kuxcchmmvdgguqgy23a7</h4>
     <img src="/coinbase-bitcoin-cash.png" class="w-100 w-50-ns" alt="">
+
+    <h3 class="mt5">ETH</h3>
+    <h4 class="courier ttn bg-dark-gray white f2 pa2 br2 break-word mv2">0x63958715F8e9Fd6CF0652394a89bb2AdD0a11686</h4>
+
+    <h3 class="mt5">LTC</h3>
+    <h4 class="courier ttn bg-dark-gray white f2 pa2 br2 break-word mv2">MDvtqRMS6QiiXpYgxtgZKHPrYnLovjdcsZ</h4>
+
+    <h3 class="mt5">XLM</h3>
+    <h4 class="courier ttn bg-dark-gray white f2 pa2 br2 break-word mv2">ejfox*keybase.io</h4>
   </section>
 </template>
 
@@ -35,4 +56,7 @@ export default {
 </script>
 
 <style>
+.break-word {
+  word-wrap: break-word;
+}
 </style>

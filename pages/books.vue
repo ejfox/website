faac1dd4ab7df3619292c4570ed96872873d17f0
EJ Fox <ejfox@ejfox.com>
2019-05-11 11:58:54 -0400

    feat: add sections and rating description to book page

--- pages/books.vue
+++ pages/books.vue
@@ -4,13 +4,36 @@
     <BookList :books="currentReadingLibrary" />
 
     <h2 class="mb2 dark-gray">Books read</h2>
+    <section class="mv3 measure">
+      <p class="mv2">I use the goodreads scale for stars.</p>
+      <ul>
+        <li>★ did not like it</li>
+        <li>★★ it was ok</li>
+        <li>★★★ liked it</li>
+        <li>★★★★ really liked it</li>
+        <li>★★★★★ it was amazing</li>
+      </ul>
+      <p class="mv2">Hovering on stars will show my review in a tooltip, if available.</p>
+    </section>
     <BookList :books="readLibrary" />
+
+    <!-- <h2 class="mb2 dark-gray">Not finished</h2>
+    <section class="mv3 measure">
+      <p>Books I've started but I'm not actively reading.</p>
+    </section>
+    <section class="gray">
+      <BookList :books="notFinishedLibrary" />
+    </section> -->
 
     <h2 class="mb2 dark-gray">Want to read</h2>
     <section class="mv3 measure">
-      <p>These are books that I've acquired or have come up in my research but that I haven't had a chance to read yet. I have a bad habit of starting books and not finishing them, and those are included here too.</p>
+      <p>These are books that I've acquired or have come up in my research but that I haven't had a chance to read yet.</p>
     </section>
-    <BookList :books="toReadLibrary" />
+    <section class="gray">
+      <BookList :books="toReadLibrary" />
+    </section>
+
+
   </section>
 </template>
 
@@ -40,6 +63,9 @@ export default {
     },
     currentReadingLibrary: function () {
       return _.filter(this.library, _.matchesProperty('Exclusive Shelf', 'currently-reading'))
+    },
+    notFinishedLibrary: function () {
+      return _.filter(this.library, _.matchesProperty('Exclusive Shelf', 'not-finished'))
     }
 
   },

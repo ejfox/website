ef4749179c422d12a8e2441c9af3c5e428134849
EJ Fox <ejfox@ejfox.com>
2019-06-24 11:54:51 -0400

    style(nav): add padding to nav on mobile

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,13 +1,13 @@
 <template>
-  <ul id="navigation" class="w-100  list pv2 pv3-ns ph0 bb bt b--dark-gray bn-ns">
+  <ul id="navigation" class="w-100 list pv3 ph1 bn">
     <!-- <li class="ph3-ns"><nuxt-link class="link underline" to="/">Home</nuxt-link></li> -->
-    <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/projects">Projects</nuxt-link></li>
+    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/projects">Projects</nuxt-link></li>
 
-    <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/bookmarks">Bookmarks</nuxt-link></li>
-    <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/books">Books</nuxt-link></li>
-    <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/vibes">Vibes</nuxt-link></li>
-    <!-- <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/creations">Creations</nuxt-link></li> -->
-    <li class="pr1 ph3-ns"><nuxt-link class="link underline blue" to="/photos">Photos</nuxt-link></li>
+    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/bookmarks">Bookmarks</nuxt-link></li>
+    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/books">Books</nuxt-link></li>
+    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/vibes">Vibes</nuxt-link></li>
+    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/creations">Creations</nuxt-link></li> -->
+    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/photos">Photos</nuxt-link></li>
   </ul>
 </template>
 

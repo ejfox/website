1f8e5ca603af110b7cb0233928f5b7c1226baa72
EJ Fox <ejfox@ejfox.com>
2019-11-10 23:36:43 -0500

    feat: add data, update resume, add json resume, update nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,12 +2,12 @@
   <ul
     role="navigation" aria-label="Site navigation"
     id="navigation" class="w-100 list pv3 ph1 bn">
-    <!-- <li class="ph3-ns"><nuxt-link class="link underline" to="/">Home</nuxt-link></li> -->
+    <li class="ph3-ns"><nuxt-link class="link underline" to="/">Home</nuxt-link></li>
     <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/projects">Projects</nuxt-link></li>
 
     <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/bookmarks">Bookmarks</nuxt-link></li> -->
-    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/books">Books</nuxt-link></li>
-    <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/vibes">Vibes</nuxt-link></li>
+    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/books">Books</nuxt-link></li> -->
+    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/vibes">Vibes</nuxt-link></li> -->
     <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/creations">Creations</nuxt-link></li> -->
     <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/photos">Photos</nuxt-link></li> -->
   </ul>

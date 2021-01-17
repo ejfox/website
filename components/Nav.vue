619aaa89ca33d6cfab9f41fd9b52bdb637bf0379
EJ Fox <ejfox@ejfox.com>
2021-01-17 01:15:21 -0500

    clean up nav page

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,10 @@
 <template>
   <ul
-    role="navigation" aria-label="Site navigation"
-    id="navigation" class="w-100 list ph3 bn mv1">
+    role="navigation"
+    aria-label="Site navigation"
+    id="navigation"
+    class="w-100 list ph3 bn mv1"
+  >
     <li class="ph3-ns pr2">
       <nuxt-link class="link underline" to="/">Home</nuxt-link>
     </li>
@@ -10,31 +13,19 @@
       <nuxt-link class="link underline " to="/projects">Projects</nuxt-link>
     </li>
 
-
     <li class="pr2 ph3-ns">
       <nuxt-link class="link underline " to="/blog">Writing</nuxt-link>
     </li>
-
-
-
-    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/bookmarks">Bookmarks</nuxt-link></li> -->
-    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/books">Books</nuxt-link></li> -->
-    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/vibes">Vibes</nuxt-link></li> -->
-    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/creations">Creations</nuxt-link></li> -->
-    <!-- <li class="pr2 ph3-ns"><nuxt-link class="link underline blue" to="/photos">Photos</nuxt-link></li> -->
   </ul>
 </template>
 
 <script scoped>
 export default {
-  data: function () {
-    return {
-    }
+  data: function() {
+    return {};
   },
-  methods: {
-  },
-  mounted: function() {
-  }
+  methods: {},
+  mounted: function() {},
 };
 </script>
 <style scoped="true">

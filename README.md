180e4074c063cc0cb97375c76878c6d284147087
EJ Fox <ejfox@ejfox.com>
2021-01-18 23:28:07 -0500

    update readme and index

--- README.md
+++ README.md
@@ -1,34 +1,41 @@
 # ejfox
+
 ## Personal website & blog for EJ Fox
+
 ### <https://ejfox.com>
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/ff495492-f06f-44e1-8986-fe4b47981237/deploy-status)](https://app.netlify.com/sites/ejfoxcom/deploys)
 
 Currently powered by:
-+ Netlify for web hosting
-+ Cloudinary for image hosting
-+ Vue/Nuxt for the site
-+ Tachyons for layout and styling
-+ Markdown for text formatting
+
+- Netlify for web hosting
+- Cloudinary for image hosting
+- Vue/Nuxt for the site
+- Tachyons for layout and styling
+- Markdown for text formatting
 
 ## Backups
+
 <http://ejfox.keybase.pub>
 
 ## To do
-+ Add status tags for posts ("notes", "draft", "in progress", "finished" )
-+ Add way for people to submit anonymous feedback / tips
-+ Add mechanism for automatically archiving linked URLs [a la Gwern](https://www.gwern.net/Archiving-URLs)
+
+- Add status tags for posts ("notes", "draft", "in progress", "finished" )
+- Add way for people to submit anonymous feedback / tips
+- Add mechanism for automatically archiving linked URLs [a la Gwern](https://www.gwern.net/Archiving-URLs)
 
 ### Books
 
 To get book library for goodreads to the books page:
-+ Go to [this page](https://www.goodreads.com/review/import) and click "Export Library"
-+ Wait for library in email
-+ Copy the goodreads CSV to the data folder
-+ Run `npx csvtojson static/data/goodreads_library_export.csv > static/data/goodreads_library_export.json` to convert the CSV into json
+
+- Go to [this page](https://www.goodreads.com/review/import) and click "Export Library"
+- Wait for library in email
+- Copy the goodreads CSV to the data folder
+- Run `npx csvtojson static/data/goodreads_library_export.csv > static/data/goodreads_library_export.json` to convert the CSV into json
 
 To scrape [highlights](https://www.goodreads.com/notes/9273959-ej-fox):
-+ `scrape-highlights https://www.goodreads.com/notes/9273959-ej-fox > static/data/book_highlights.json`
 
+- `scrape-highlights https://www.goodreads.com/notes/9273959-ej-fox > static/data/book_highlights.json`
 
 ### Markdown Blog Posts
 
@@ -49,8 +56,42 @@ processmd content/blog/md/* --outputDir content/blog/posts/
 npm run blog:md-to-json
 ```
 
+## How to make a new photo post
+
+Make sure the following environment variables are set in your .env file:
+
+- CLOUDINARY_CLOUD_NAME
+- CLOUDINARY_API_KEY
+- CLOUDINARY_API_SECRET
+
+As of Jan 2021, all 3 of these values can be found by logging into
+https://cloudinary.com
+
+If starting in Lightroom, you want to go through each photo and make sure it
+has a caption. You can add the caption in the Metadata section on the
+right side of the screen when in Library mode. These captions will automatically
+be displayed when processed with the script. This is basically the only way to
+get text into photo blog posts. 
+
+Once you have gathered all of the photos you would like to publish, select
+them all and export them to a folder. Take note of the path of this folder
+with a tool like `pwd`, as we will need to pass it to our script.
+
+In the root of this website, you want to run
+```bash
+node folder-to-blogpost.js ~/PATH/TO/IMAGES/
+```
+
+This will take every photo and process it by uploading the image to
+Cloudinary, and then writing the public uploaded Clouindary URL as well
+as the relevant EXIF data (most importantly captions) and writes it to a 
+JSON file. That JSON file will be moved to `content/photos/` so it can be
+displayed as a post on the photoblog, which checks for all .json files
+located in that directory.
+
 ## Build Setup
-``` bash
+
+```bash
 # install dependencies
 npm install # Or yarn install
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,46 @@
 <template>
-	<section class="pa3 pa4-ns db cf mb4">
-		<section class="intro pt2 measure-narrow lh-copy tj">
-			<p class="pv2">
-				Hello, my name is <strong>EJ Fox</strong>. I'm a
-				hacker and journalist available for contract
-				work. My work focuses on data visualization,
-				systems, and politics. You can send me an email
-				at
-				<a href="mailto:ejfox@ejfox.com"
-					>ejfox@ejfox.com</a
-				>
-				
-			</p>
+  <section class="pa3 pa4-ns db cf mb4">
+    <section class="intro pt2 measure-narrow lh-copy tj">
+      <p class="pv2">
+        Hello, my name is <strong>EJ Fox</strong>. I'm a hacker and journalist
+        available for contract work. My work focuses on data visualization,
+        systems, and politics. You can send me an email at
+        <a href="mailto:ejfox@ejfox.com">ejfox@ejfox.com</a>
+      </p>
 
-			<p class="tl">
-				<nuxt-link to="/projects">
-					Take a look at some of my work
-				</nuxt-link>
-				or <a href="/resume.pdf">my resume</a>.
-			</p>
-			<p class="lh-copy pv2 tj">
-				Sometimes I take
-				<nuxt-link to="/photo/">photos</nuxt-link>, make
-				<nuxt-link to="/audio/">sounds</nuxt-link>, or
-				read <nuxt-link to="/books/">books</nuxt-link>.
-			</p>
-		</section>
-		<Footer />
+      <p class="tl">
+        <nuxt-link to="/projects">
+          Take a look at some of my work
+        </nuxt-link>
+        or <a href="/resume.pdf">my resume</a>.
+      </p>
+      <p class="lh-copy pv2 tj">
+        Sometimes I take
+        <nuxt-link to="/photo/">photos</nuxt-link>, make
+        <nuxt-link to="/audio/">sounds</nuxt-link>, or read
+        <nuxt-link to="/books/">books</nuxt-link>.
+      </p>
+    </section>
+    <Footer />
 
-		<div id="hcard-EJ-Fox" class="vcard w-100 f6 tc dn">
-			<img
-				src="https://gravatar.com/avatar/4a503ee102c67cc632d77f97721d83f7"
-				alt="Gravatar photo of EJ Fox"
-				class="photo dn"
-			/>
-			<a class="url fn db" href="https://ejfox.com">EJ Fox</a>
-			<a class="email db" href="mailto:ejfox@ejfox.com"
-				>ejfox@ejfox.com</a
-			>
-			<div class="adr">
-				<span class="locality">Beacon</span>,
-				<span class="region">New York</span>
+    <div id="hcard-EJ-Fox" class="vcard w-100 f6 tc dn">
+      <img
+        src="https://gravatar.com/avatar/4a503ee102c67cc632d77f97721d83f7"
+        alt="Gravatar photo of EJ Fox"
+        class="photo dn"
+      />
+      <div><a class="url fn db" href="https://ejfox.com">EJ Fox</a></div>
+      <a class="email db" href="mailto:ejfox@ejfox.com">ejfox@ejfox.com</a>
+      <div class="adr">
+        <span class="locality">Beacon</span>,
+        <span class="region">New York</span>
 
-				<span class="country-name ml1 b"
-					>United States</span
-				>
-			</div>
+        <div class="country-name ml1 b">United States</div>
+      </div>
 
-			<div class="tel sans-serif">(260) 673-5369</div>
-		</div>
-	</section>
+      <div class="tel sans-serif">(260) 673-5369</div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -57,50 +48,44 @@ import Nav from "~/components/Nav.vue";
 import Footer from "~/components/Footer.vue";
 
 export default {
-	components: {
-		Nav,
-		Footer,
-	},
-	methods: {},
-	data() {
-		// Using webpacks context to gather all files from a folder
-		const context = require.context(
-			"~/content/blog/posts/",
-			false,
-			/\.json$/
-		);
+  components: {
+    Nav,
+    Footer,
+  },
+  methods: {},
+  data() {
+    // Using webpacks context to gather all files from a folder
+    const context = require.context("~/content/blog/posts/", false, /\.json$/);
 
-		let posts = context.keys().map((key) => ({
-			...context(key),
-			_path: `/blog/${key
-				.replace(".json", "")
-				.replace("./", "")}`,
-		}));
+    let posts = context.keys().map((key) => ({
+      ...context(key),
+      _path: `/blog/${key.replace(".json", "").replace("./", "")}`,
+    }));
 
-		posts = posts.sort(function(a, b) {
-			return new Date(b.date) - new Date(a.date);
-		});
-		posts = posts.filter((post) => !post.hidden);
-		return { posts };
-	},
+    posts = posts.sort(function(a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
+    posts = posts.filter((post) => !post.hidden);
+    return { posts };
+  },
 };
 </script>
 
 <style scoped>
 ul {
-	padding: 0;
+  padding: 0;
 }
 /* li { list-style-type: none } */
 
 .intro a:link,
 .intro a:hover {
-	color: black;
-	text-decoration: underline;
+  color: black;
+  text-decoration: underline;
 }
 
 .intro a:visited,
 .intro a:focus {
-	color: #414346;
-	/*text-decoration: none;*/
+  color: #414346;
+  /*text-decoration: none;*/
 }
 </style>

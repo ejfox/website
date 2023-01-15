212a1589bdb354a487117a7461e091d44955f4e6
EJ Fox <ejfox@ejfox.com>
2023-01-15 15:44:09 -0500

    fix: behavior for hash IDs; go to hash when there is one, otherwise scroll to top on page change

--- plugins/scroll-top.ts
+++ plugins/scroll-top.ts
@@ -1,5 +1,13 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("page:finish", async (a) => {
-    window.scrollTo({ top: 0 });
+    // check if the route includes a hash, and if it does not, scroll to the top of the page
+    if (!window.location.hash) window.scrollTo({ top: 0 });
+
+    // otherwise scroll to the element with the id of the hash
+    else {
+      const hash = window.location.hash;
+      const element = document.querySelector(hash);
+      if (element) element.scrollIntoView();
+    }
   });
 });

bd0ba3d8e87b3185c87ede4c8e28e4f7badae46d
EJ Fox <ejfox@ejfox.com>
2023-01-15 13:55:00 -0500

    style: better list styling

--- components/content/ProseUl.vue
+++ components/content/ProseUl.vue
@@ -3,3 +3,22 @@
     <slot />
   </ul>
 </template>
+<style>
+/* style nested lists */
+ul ul {
+  margin-left: 2.25em !important;
+  width: 50%;
+
+}
+
+ul ul li {
+  list-style-type: square;
+}
+
+/* remove bottom border from last li in nested list */
+ul ul li:last-child {
+  border-bottom: none;
+}
+
+
+</style>

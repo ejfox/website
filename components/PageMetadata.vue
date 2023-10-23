f9c5aec3a34c27d33284d81b0f34e98bd8577792
EJ Fox <ejfox@ejfox.com>
2023-10-23 12:22:50 -0400

    refactor(PageMetadata): fix pluralization of "photos"

--- components/PageMetadata.vue
+++ components/PageMetadata.vue
@@ -35,7 +35,7 @@
       </span>
       <span class="mr-4 whitespace-nowrap inline-block" v-if="countPhotos(doc) > 0">
         <UIcon name="ant-design:camera-filled" class="mr-1 text-sm pb-1" />
-        {{ countPhotos(doc) }} photos
+        {{ countPhotos(doc) }} photo<span v-if="+countPhotos(doc) > 1">s</span>
       </span>
 
       <span class="mr-4 whitespace-nowrap inline-block" v-if="countLinks(doc) > 1">

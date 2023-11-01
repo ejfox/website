0c9c3fe6347b0571b3d9c7835da2813286eb1c9b
EJ Fox <ejfox@ejfox.com>
2023-11-01 19:57:50 -0400

    feat: hello 3D canvas

---
+++ pages/scrapbook/canvas3d.vue
@@ -0,0 +1,23 @@
+<script setup>
+const { onLoop } = useRenderLoop()
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <TresCanvas window-size>
+      <TresPerspectiveCamera :position="[5, 5, 5]" />
+      <OrbitControls />
+      <TresMesh :position="[-2, 2, 0]" :rotation="[0, Math.PI, 0]">
+        <TresConeGeometry :args="[1, 1.5, 3]" />
+        <TresMeshToonMaterial color="#82DBC5" />
+      </TresMesh>
+      <EffectComposer>
+        <Bloom />
+      </EffectComposer>
+      <TresAmbientLight :intensity="1" />
+      <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" />
+    </TresCanvas>
+  </div>
+</template>

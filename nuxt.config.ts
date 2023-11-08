9127a3fce03cb78e92fe329ce977743f7c5eb220
EJ Fox <ejfox@ejfox.com>
2023-11-08 17:36:26 -0500

    build: potential fix for Netlify build error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,4 +108,14 @@ export default {
       ],
     },
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+        cssnano:
+          process.env.NODE_ENV === 'production'
+            ? { preset: ['default', { discardComments: { removeAll: true } }] }
+         : false, // disable cssnano when not in production
+    },
+  }
 }

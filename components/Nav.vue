3d80b2611ec96e7b08ffba7a35a90ef162da34f9
EJ Fox <ejfox@ejfox.com>
2023-11-27 18:06:57 -0500

    Update Nav.vue component styling

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,17 +1,18 @@
 <template>
-  <nav
-    class="bg-neutral-50 dark:bg-gray-900 drop-shadow-sm py-1 xl:py-2 sticky top-0 md:static md:top-auto bg-opacity-90 backdrop-blur-sm z-50">
-    <div class="container mx-auto py-1 flex justify-between items-center">
-      <NuxtLink class="text-gray-800 dark:text-white text-xl font-bold pl-2" to="/">EJ Fox</NuxtLink>
-      <div class="">
-        <NuxtLink active-class="text-primary-500" class="pr-2 md:px-4 py-2" to="/">Home
+  <nav class="p-4 xl:py-2 sticky top-0 md:static md:top-auto bg-opacity-90 backdrop-blur-sm z-50">
+    <div class="container mx-auto py-1 flex flex-col items-start"> <!-- Updated class: items-start -->
+      <NuxtLink class="text-gray-800 dark:text-white text-xl font-bold p-4" to="/">EJ Fox</NuxtLink>
+
+      <div class="space-y-2">
+        <NuxtLink active-class="text-primary-500" class="block w-full px-4 py-2 text-lg md:text-xl" to="/">
+          Home
         </NuxtLink>
-        <NuxtLink active-class="text-primary-500" class="pr-2 md:px-4 py-2" to="/projects">
-          Projects</NuxtLink>
-        <NuxtLink active-class="text-primary-500" class="pr-2 md:px-4 py-2" to="/blog/">Blog
+        <NuxtLink active-class="text-primary-500" class="block w-full px-4 py-2 text-lg md:text-xl" to="/projects">
+          Projects
         </NuxtLink>
-        <!-- <NuxtLink active-class="text-primary-500" class="pr-2 md:px-4 py-2" to="/scrapbook/">
-          Scrapbook</NuxtLink> -->
+        <NuxtLink active-class="text-primary-500" class="block w-full px-4 py-2 text-lg md:text-xl" to="/blog/">Blog
+        </NuxtLink>
+        <!-- <NuxtLink active-class="text-primary-500" class="md:px-4 py-2" to="/scrapbook/">Scrapbook</NuxtLink> -->
       </div>
     </div>
   </nav>

4e364745ca6bfe106df0bba3ee0e85161dbb35a0
EJ Fox <ejfox@ejfox.com>
2023-12-27 18:59:06 -0500

    feat: add ProjectStatus component
    Use like:
    
    ```md
    ::project-status{totalBubbles=10 filledBubbles=4}
    ::
    ```

---
+++ components/content/ProjectStatus.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="project-status flex">
+    <div v-for="n in totalBubbles" :key="n" class="bubble flex items-center justify-center"
+      :class="{ 'bg-primary-500': n <= filledBubbles }"></div>
+  </div>
+</template>
+
+<style scoped>
+.bubble {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #e2e8f0;
+  margin-right: 5px;
+}
+
+.bg-blue-500 {
+  background-color: #4299e1;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    totalBubbles: {
+      type: Number,
+      default: 10
+    },
+    filledBubbles: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bubble {
+  /* Styles for bubbles */
+}
+
+.bubble.filled {
+  /* Styles for filled bubbles */
+}
+</style>

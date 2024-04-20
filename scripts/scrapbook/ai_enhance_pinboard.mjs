6735b07e178ac1219d19b22820bc26256c8c08ae
EJ Fox <ejfox@ejfox.com>
2024-04-20 17:06:26 -0400

    refactor: scaffold out using anthropic to summarize pinboard bookmarks for scrapbook

--- scripts/scrapbook/ai_enhance_pinboard.mjs
+++ scripts/scrapbook/ai_enhance_pinboard.mjs
@@ -2,6 +2,8 @@
 // Load up the bookmarks JSON data
 import { fetchAndSummarizeUrl } from '@ejfox/gpt-browser'
 
+const USER_TAGS = `3d, art, cli, data, dataviz, design, elections, hackers, howto, javascript, journalism, machine learning / AI, mapping/cartography, music, police, politics, protest, tech, tool, video, vj, web development, creative coding, outdoors, motorcycles, or travel`
+
 // const fs = require('fs')
 import fs from 'fs'
 const bookmarksData = JSON.parse(
@@ -16,7 +18,10 @@ const groupedData = d3.group(bookmarksData, (d) =>
   d3.timeWeek(new Date(d.time)),
 )
 
-// make a big for loop to process the data one by one
+/**
+ * Add a summary to each bookmark
+ * @returns {Promise<void>}
+ */
 async function addSummaryToBookmarks() {
   for (const bookmark of bookmarksData) {
     console.log(`Summarizing ${bookmark.href}...`)
@@ -54,7 +59,17 @@ async function addSummaryToBookmarks() {
   }
 }
 
+/**
+ * Adds a meta summary to bookmarks, which is a summary of all the bookmarks from the week
+ * @returns 
+ */
 async function addMetaSummaryToBookmarks() {
+  // This regenerates the bookmark summary, but taking into context the summaries of *all* bookmarks from the week
+  // This is useful for generating a summary of the week's research
+
+
+  const completionService = 'openai'; // or 'anthropic'
+
   for (const bookmark of bookmarksData) {
     // now we need to re-summarize given the context of the other bookmarks from this week
     const timeDate = new Date(bookmark.time)
@@ -67,7 +82,7 @@ async function addMetaSummaryToBookmarks() {
       continue
     }
 
-    // combine all the summaries
+    // combine all the summaries for this week
     const allSummaries = bookmarksThisWeek
       .map((b) => `# <${b.href}> ${b.description}\n\n${b.summary}`)
       .join(`\n`)
@@ -76,56 +91,67 @@ async function addMetaSummaryToBookmarks() {
 
     // console.log('allSummaries: ', allSummaries)
 
-    // re-summarize
-    const summaryAgainCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content: `Embrace the archives of the user's digital journey, sifting through bookmarked realms to distill essences of knowledge and inspiration.`,
-        },
-        {
-          role: 'user',
-          content: `Can you help me summarize an article, tying it into my ongoing research for the week? Usually it fits into one of these tags: 3d, art, cli, data, dataviz, design, elections, hackers, howto, javascript, journalism, machine learning / AI, mapping/cartography, music, police, politics, protest, tech, tool, video, vj, web development, creative coding, outdoors, motorcycles, or travel. If it's a new topic, I'll add a new tag.
-          
-          Here are some examples of good summaries:
-          - "An investigation by Mission Local into the influence of tech and real estate "big money" on San Francisco"
-          - "A GitHub repo that helps users create a custom map of their own city"
-          `,
-        },
-        {
-          role: 'assistant',
-          content: `Absolutely! Please send along the article summary and all of the bookmark and I'll do my best to summarize the topic and tie it into the ongoing research for the week.`,
-        },
-        {
-          role: 'user',
-          content: `## This week's research\n\n${allSummaries}\n\n## Article Summary\n\n${bookmark.summary}\n\n## Research question\n\nPlease summarize this information and tie it into the ongoing research for the week, if possible. Try to be short, factual, and concise, with the tone of a journalist. Please keep your responses as short as possible, under 2 short sentences. Never repeat the week's theme, focus on the Article Summary and relating it to the ongoing research or the tags I use. No chit-chat or preamble, skip directly into the description. Never talk about "this week" - focus on the bookmark. Stick to the facts.`,
-        },
-      ],
-      // n: 3, // get 3 completions
-      model: 'gpt-4-turbo-preview',
-    })
+    const resummarizeMessages = [
+      {
+        role: 'system',
+        content: `Embrace the archives of the user's digital journey, sifting through bookmarked realms to distill essences of knowledge and inspiration.`,
+      },
+      {
+        role: 'user',
+        content: `Can you help me summarize an article, tying it into my ongoing research for the week? Usually it fits into one of these tags: ${USER_TAGS}. If it's a new topic, I'll add a new tag.
+        
+        Here are some examples of good summaries:
+        - "An investigation by Mission Local into the influence of tech and real estate "big money" on San Francisco- ties into recent research on the emerging right-wing tech industry and the dystopian impact of tech on society"
+        - "A GitHub repo that helps users create a custom map of their own city, typing into ongoing research on mapping tools, potentially related to recent research on San Francisco's housing crisis"
+        `,
+      },
+      {
+        role: 'assistant',
+        content: `Absolutely! Please send along the article summary and all of the bookmark and I'll do my best to summarize the topic and tie it into the ongoing research for the week.`,
+      },
+      {
+        role: 'user',
+        content: `## This week's research\n\n${allSummaries}\n\n## Article Summary\n\n${bookmark.summary}\n\n## Research question\n\nPlease summarize this information and tie it into the ongoing research for the week, if possible. Try to be short, factual, and concise, with the tone of a journalist. Please keep your responses as short as possible, under 2 short sentences. Never repeat the week's theme, focus on the Article Summary and relating it to the ongoing research or the tags I use. No chit-chat or preamble, skip directly into the description. Never talk about "this week" - focus on the bookmark. Stick to the facts.`,
+      },
+    ]
 
-    // console log all the completions
-    // console.log(summaryAgainCompletion.choices)
+    let summaryAgainCompletion;
+    if (completionService === 'openai') {
+      summaryAgainCompletion = await openai.chat.completions.create({
+        messages: resummarizeMessages,
+        model: 'gpt-4-turbo-preview',
+      }).choices[0]
+    } else if (completionService === 'anthropic') {
+      // anthropic requires alternating user/assistant messages ONLY
+      // so we need to turn any system messages into user messages
+      // and collapse any user/system messages that are back-to-back
+      const messages = resummarizeMessages.reduce((acc, msg) => {
+        if (msg.role === 'system') {
+          acc.push({ role: 'user', content: msg.content });
+        } else if (acc.length > 0 && acc[acc.length - 1].role === 'user') {
+          acc[acc.length - 1].content += '\n\n' + msg.content;
+        } else {
+          acc.push({ role: 'assistant', content: msg.content });
+        }
+        return acc;
+      }, []);
 
-    // console.log(summaryAgainCompletion.choices[0]);
-    // find whichever completion is shortest
-    // const bestCompletion = summaryAgainCompletion.choices.reduce(
-    //   (best, current) => {
-    //     if (current.text.length < best.text.length) {
-    //       return current
-    //     } else {
-    //       return best
-    //     }
-    //   },
-    // )
 
-    const bestCompletion = summaryAgainCompletion.choices[0]
+      const msg = await anthropic.messages.create({
+        model: "claude-3-opus-20240229",
+        max_tokens: 1024,
+        messages,
+      });
 
-    console.log('bestCompletion: ', bestCompletion)
+      summaryAgainCompletion = msg;
+    }
+
+    console.log(summaryAgainCompletion)
+
+    // const summaryCompletion = summaryAgainCompletion.choices[0]
 
     // add the new summary to the bookmark
-    bookmark.summaryAgain = bestCompletion.message.content
+    bookmark.summaryAgain = summaryCompletionAgainCompletion
 
     // write the updated bookmarks data to a file
     fs.writeFileSync(
@@ -160,32 +186,34 @@ async function summarizeWeeks() {
       )
       .join(`\n`)
 
+    const messages = [
+      {
+        role: 'system',
+        content: `Embrace the archives of the user's digital journey, sifting through bookmarked realms to distill essences of knowledge and inspiration- help the user identify the sometimes hidden threads in their research, looking for small connections to big ideas and surfacing details that tie everything together.`,
+      },
+      {
+        role: 'user',
+        content: `Can you help me identify the over-arching threads between a bunch of bookmarks I made this week? Usually it fits into one of these tags: ${USER_TAGS}. 
+
+        Try to use a narrative approach:
+        "This week's research focused on logistics, with an emphasis on radios and knots. A story about a new tool for creating custom maps was also found, and might be related to the logistics theme. Many articles tie into outdoors and travel."
+
+        "This week's research primarily focused on elections and politics, with a few articles about police and protest, including one about an officer who was fired for speaking out against police brutality. On Wednesday, 2 new mapping tools were identified."
+        `,
+      },
+      {
+        role: 'assistant',
+        content: `Absolutely! Please send along the article summaries from your bookmarks this week and I can get to synthesizing the information.`,
+      },
+      {
+        role: 'user',
+        content: `## This week's research\n\n${allSummaries}\n\n## Research question\n\nPlease summarize this information and identify the research themes of the week. No chit-chat or preamble, skip directly into the description. Keep it short, concise, and information-dense. Do not respond in the form of a list- try to keep it to 4-6 sentences. Avoid adjectives or self-aggandizing language. Stick to the facts.`,
+      },
+    ]
+
     // re-summarize
     const summaryAgainCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content: `Embrace the archives of the user's digital journey, sifting through bookmarked realms to distill essences of knowledge and inspiration- help the user identify the sometimes hidden threads in their research, looking for small connections to big ideas and surfacing details that tie everything together.`,
-        },
-        {
-          role: 'user',
-          content: `Can you help me identify the over-arching threads between a bunch of bookmarks I made this week? Usually it fits into one of these tags: 3d, art, cli, data, dataviz, design, elections, hackers, howto, javascript, journalism, machine learning / AI, mapping/cartography, music, police, politics, protest, tech, tool, video, vj, web development, creative coding, outdoors, motorcycles, or travel. 
-
-          Try to use a narrative approach:
-          "This week's research focused on logistics, with an emphasis on radios and knots. A story about a new tool for creating custom maps was also found, and might be related to the logistics theme. Many articles tie into outdoors and travel."
-
-          "This week's research primarily focused on elections and politics, with a few articles about police and protest, including one about an officer who was fired for speaking out against police brutality. On Wednesday, 2 new mapping tools were identified."
-          `,
-        },
-        {
-          role: 'assistant',
-          content: `Absolutely! Please send along the article summaries from your bookmarks this week and I can get to synthesizing the information.`,
-        },
-        {
-          role: 'user',
-          content: `## This week's research\n\n${allSummaries}\n\n## Research question\n\nPlease summarize this information and identify the research themes of the week. No chit-chat or preamble, skip directly into the description. Keep it short, concise, and information-dense. Do not respond in the form of a list- try to keep it to 4-6 sentences. Avoid adjectives or self-aggandizing language. Stick to the facts.`,
-        },
-      ],
+      messages,
       // n: 3, // get
       model: 'gpt-4-turbo-preview',
     })
@@ -205,4 +233,4 @@ async function summarizeWeeks() {
   }
 }
 
-await summarizeWeeks()
+await addSummaryToBookmarks().then(() => addMetaSummaryToBookmarks()).then(() => summarizeWeeks())

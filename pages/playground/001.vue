e622e22ccf3516182ec6f5006cc2a1d5f05e3eb8
EJ Fox <ejfox@ejfox.com>
2024-05-10 10:34:55 -0400

    feat: add playground 'ehllo world'

---
+++ pages/playground/001.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-900">
+    <h1>Playground</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>

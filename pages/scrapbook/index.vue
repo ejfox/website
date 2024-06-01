5bef667d97e2b3fef05573807cda0e1c6e12c648
EJ Fox <ejfox@ejfox.com>
2024-06-01 19:34:39 -0400

    chore(scrapbook): Update scrapbook page with horizontally scrolling deck drawer and vertically scrolling deck drawer

--- pages/scrapbook/index.vue
+++ pages/scrapbook/index.vue
@@ -11,29 +11,74 @@
       <ScrapGallery v-if="group.type === 'gallery'" :scraps="group.items" />
       <ScrapPRBlock v-else-if="group.type === 'pr'" :scraps="group.items" />
       <ScrapItem v-else :scrap="group.items[0]" />
-      <UButton @click="addScrapToDeck(group.items[0])" color="green">Add</UButton>
-      <UButton @click="removeScrapFromDeck(group.items[0])" color="red">Remove</UButton>
+
+      <UButton v-if="scrapOnDeck.includes(group.items[0].scrap_id)" @click="removeScrapFromDeck(group.items[0])"
+        color="red">Remove</UButton>
+      <UButton v-else @click="addScrapToDeck(group.items[0])" color="green">Add to deck</UButton>
     </div>
     <div v-if="loading" class="text-center">Loading...</div>
 
 
-    <!-- the little on deck drawer -->
-    <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 h-80 overflow-x-auto overflow-y-hidden"
+    <!-- horizontally scrolling deck drawer -->
+    <!-- <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-400 h-80 overflow-x-auto overflow-y-hidden"
       v-if="scrapOnDeck.length > 0">
-      <!-- <pre>{{ scrapOnDeckObjects }}</pre> -->
-
-      <div class="p-4 w-full">
+      <div class="px-2 py-1 w-full">
         <h2 class="text-lg font-bold m-0 py-1">On Deck
           <span class="mr-2">
             <UBadge color="black">{{ scrapOnDeck.length }}</UBadge>
           </span>
           <UButton @click="clearDeck" color="red">Clear all</UButton>
+
+
+
         </h2>
         <div v-if="scrapOnDeck.length === 0" class="text-center text-gray-500">No scraps on deck</div>
-        <div v-else class="flex flex-nowrap overflow-x-auto">
+        <div v-else class="flex flex-nowrap overflow-x-auto ">
           <div v-for="scrap in scrapOnDeckObjects" :key="scrapId"
-            class="border-b border-gray-200 bg-gray-200 dark:bg-gray-800 p-1 rounded-sm min-h-48 min-w-48 mr-2">
+            class="border-b border-gray-200 bg-gray-200 dark:bg-gray-800 rounded-sm min-h-48 min-w-48 mr-2">
             <ScrapVerboseScrapItem v-if="scrap" :scrap="scrap" class="min-h-32" />
+            <UButton @click="removeScrapFromDeck(scrap.scrap_id)" color="red" variant="outline">Remove</UButton>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <!-- vertically scrolling deck drawer -->
+    <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-400 h-80 overflow-y-auto"
+      v-if="scrapOnDeck.length > 0">
+      <div class="px-2 py-1 w-full">
+        <h2 class="text-lg font-bold m-0 py-1">On Deck
+          <span class="mr-2">
+            <UBadge color="black">{{ scrapOnDeck.length }}</UBadge>
+          </span>
+          <UButton @click="clearDeck" color="red" class="m-1">Clear all</UButton>
+
+          <UButton @click="addScrapByIdMode = !addScrapByIdMode" color="yellow" class="m-1">Add by ID</UButton>
+
+          <!-- save / load deck buttons -->
+          <UButton @click="saveDeck" color="green" class="m-1">Save as deck
+            {{ currentDeck + 1 }}
+          </UButton>
+          <div class="inline-flex flex-row">
+            <UButton v-for="(deck, index) in savedDecks" @click="loadDeck(index)" color="blue" class="mx-1">
+              Load deck {{ index }}
+
+              <UIcon name="i-ic-baseline-delete-forever" @click="removeDeck(index)" class="ml-" />
+            </UButton>
+          </div>
+          <!-- button to open up add by ID mode -->
+
+
+          <div v-if="addScrapByIdMode" class="flex flex-row py-2">
+            <UInput v-model="scrapIdToAdd" placeholder="Scrap ID" />
+            <UButton @click="addScrapByIdToDeck(scrapIdToAdd)" color="green" class="mx-1">Add scrap</UButton>
+          </div>
+        </h2>
+        <div v-if="scrapOnDeck.length === 0" class="text-center text-gray-500">No scraps on deck</div>
+        <div v-else class="p-1 md:px-4 lg:px-8 ">
+          <div v-for="scrap in scrapOnDeckObjects" :key="scrapId"
+            class="border-b border-gray-200 bg-gray-200 dark:bg-gray-800 p-1 md:p-2 lg:px-4 rounded-sm w-full py-2 my-2 flex flex-row justify-between">
+            <ScrapVerboseScrapItem v-if="scrap" :scrap="scrap" class="leading-none" />
             <UButton @click="removeScrapFromDeck(scrap.scrap_id)" color="red" variant="outline">Remove</UButton>
           </div>
         </div>
@@ -62,6 +107,9 @@ const displayedData = ref([])
 const loading = ref(false)
 const PAGE_SIZE = 20
 
+const scrapIdToAdd = ref('')
+const addScrapByIdMode = ref(false)
+
 
 // we are gonna make a sort of, ya know, scrap shopping cart
 // in the first step of letting the user put together threads
@@ -71,9 +119,29 @@ const PAGE_SIZE = 20
 // so we can grab it on other pages
 
 const scrapOnDeck = useLocalStorage('scrapOnDeck', [])
-
-
 const scrapOnDeckObjects = ref([])
+
+// and finally, an array to save our decks to and load them from later
+const savedDecks = useLocalStorage('savedDecks', [])
+
+// decks don't have names, they just auto-increment
+const currentDeck = useLocalStorage('currentDeck', 0)
+
+const saveDeck = () => {
+  savedDecks.value.push(scrapOnDeck.value)
+  currentDeck.value = savedDecks.value.length - 1
+  scrapOnDeck.value = []
+}
+
+const loadDeck = (deckIndex) => {
+  scrapOnDeck.value = savedDecks.value[deckIndex]
+  currentDeck.value = deckIndex
+}
+
+const removeDeck = (deckIndex) => {
+  savedDecks.value.splice(deckIndex, 1)
+}
+
 
 watchEffect(() => {
   if (!combinedData.value) return
@@ -91,6 +159,14 @@ const addScrapToDeck = (scrapId) => {
     scrapId = scrapId.scrap_id
   }
   scrapOnDeck.value.push(scrapId)
+}
+
+function addScrapByIdToDeck(scrapId) {
+  // this should close the add by ID mode as well as add the scrap to the deck
+  addScrapToDeck(scrapId)
+  addScrapByIdMode.value = false
+  scrapIdToAdd.value = ''
+
 }
 
 const removeScrapFromDeck = (scrapId) => {

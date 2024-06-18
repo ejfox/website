262278add91fd5bff983879d142981a0d9e59ff9
EJ Fox <ejfox@ejfox.com>
2024-06-18 14:08:02 -0400

    scaffold: add a page where I recommend RSS and Podcast feeds

---
+++ pages/feeds.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>
+      Feeds I recommend
+    </h2>
+    <div v-for="feed in feeds">
+      <div>{{ feed.title }}</div>
+      <div>{{ feed.xmlUrl }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+
+// parse an OPML file in the public folder
+const opmlPath = "/rss_feeds.opml"
+const podcastOpmlPath = "/podcast_feeds.opml"
+
+const { data } = useFetch(opmlPath)
+const { data: podcastData } = useFetch(podcastOpmlPath)
+
+// parse the OPML data
+const feeds = parseOpmlToFeed(data.value)
+const podcastFeeds = parseOpmlToFeed(podcastData.value)
+
+function parseOpmlToFeed(data) {
+  // parse the OPML data
+  // return an array of feed objects
+  return []
+}
+
+
+</script>
+
+<style scoped></style>

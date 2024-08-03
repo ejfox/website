20625f4d4f5ae15df8efcc5110a4657dc64d863f
EJ Fox <ejfox@ejfox.com>
2024-08-03 12:44:02 -0400

    style: tweak display of pottery products

--- pages/pottery/index.client.vue
+++ pages/pottery/index.client.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="p-4 lg:px-12">
-    <img src="/images/handdrawn_ceramics_text/handdrawn_ceramics_text-01.svg"
-      class="p-2 md:p-4 lg:p-16 mt-4 w-64 h-auto dark:invert" />
-
-    <img src="/images/handdrawn_ceramics_text/handdrawn_ceramics_text-24.svg" class="w-16 md:w-32 h-auto
+  <div class="p-4 lg:px-8">
+    <img src="/images/handdrawn_ceramics_text/handdrawn_ceramics_text-24.svg" class="w-8 md:w-12 h-auto
+    mx-auto
     absolute
-    top-4
-    right-4 md:right-8
+    z-50
+    top-2
+    right-4
     dark:invert" />
 
     <img src="/images/handdrawn_ceramics_text/handdrawn_ceramics_text-26.svg"
-      class="w-full p-4 h-auto dark:invert max-w-screen-md  mb-4" />
+      class="w-full p-4 h-auto dark:invert max-w-screen-md  mb-4 mx-auto" />
 
 
     <div v-if="!products" class="flex justify-center items-center h-64">
       <UProgress />
     </div>
-    <div v-for="product in products" :key="product.id" class="p-2 max-w-screen-md flex flex-col my-8">
-      <StripeProduct :product="product" />
+    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-4 lg:gap-6 xl:gap-8 my-8">
+
+
+      <div v-for="product in products" :key="product.id" class="p-2 max-w-screen-md ">
+        <StripeProduct :product="product" />
+      </div>
     </div>
+
+
+
 
     <img :src="`/images/handdrawn_ceramics_text/handdrawn_ceramics_text-09.svg`"
       class="mt-4 w-24 mx-auto h-auto dark:invert" />
